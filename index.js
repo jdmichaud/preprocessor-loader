d14f19f133fbf08b829a063201e146dd0ce2f9c2
JD <jeandaniel.michaud@ge.com>
2016-11-09 14:51:00 +0100

    Add user defined macro expension without or with parameters.

--- index.js
+++ index.js
@@ -1,44 +1,93 @@
+/* eslint prefer-template: 0 */
+/* eslint no-useless-escape: 0 */
+
 /*
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author jean.daniel.michaud@gmail.com
 */
-var loaderUtils = require("loader-utils");
+var fs = require('fs');
+var loaderUtils = require('loader-utils');
 
-module.exports = function(content) {
-  this.cacheable && this.cacheable();
-  if(!this.emitFile) throw new Error("emitFile is required from module system");
+function replaceFilename(content, filename) {
+  return content.replace(/__FILE__/g, filename);
+}
+
+function replaceLinenumber(content) {
+  return content
+          .split('\n')
+          .map((line, index) => line.replace(/__LINE__/g, index + 1))
+          .join('\n');
+}
+
+function preprocessMacros(macros) {
+  macros.forEach((macro) => {
+    const split = macro.declaration.split(/[(),]/).filter(Boolean);
+    macro.name = split[0].trim();
+    macro.args = split.slice(1).map(arg => arg.trim());
+  });
+  return macros;
+}
+
+function buildSubstitution(expression, macro) {
+  const split = expression.split(/[(),]/).filter(Boolean);
+  let result = macro.definition;
+  macro.args.forEach((arg, index) => {
+    result = result.replace(new RegExp(arg, 'g'), split[index + 1]);
+  });
+  return result;
+}
+
+function replaceMacros(content, macros) {
+  var result = content;
+  macros.forEach((macro) => {
+    if (macro.args.length === 0) {
+      // Macro has no arguments, easy
+      result = result.replace(new RegExp(macro.name + '\s*(\\(\\))?', 'g'),
+                              macro.definition);
+    } else {
+      // Macro has arguments
+      // First retrieve the expressions to replace
+      const expressions = result.match(new RegExp(macro.name + '\s*\\([^\)]+\\)', 'g'));
+      if (expressions) {
+        // Then extract the parameters
+        expressions.forEach((expression) => {
+          // Build the definition with variable substitution
+          const substitution = buildSubstitution(expression, macro);
+          // Finally replace the declaration by the definition
+          result = result.replace(expression, substitution);
+        });
+      }
+    }
+  });
+  return result;
+}
+
+module.exports = function (content) {
+  if (this.cacheable) this.cacheable();
+  if (!this.emitFile) throw new Error('emitFile is required from module system');
 
   var query = loaderUtils.parseQuery(this.query);
-  var configKey = query.config || "preprocessorLoader";
-  var options = this.options[configKey] || {};
+  var config = {};
 
-  var config = {
-  };
+  // Load config files
+  if (query.config !== undefined && query.config !== '') {
+    config = JSON.parse(fs.readFileSync(query.config, 'utf8'));
+  }
 
-  // options takes precedence over config
-  Object.keys(options).forEach(function(attr) {
-    config[attr] = options[attr];
-  });
-
-  // query takes precedence over config and options
-  Object.keys(query).forEach(function(attr) {
-    config[attr] = query[attr];
-  });
-
+  // Replace user defined macros
+  if (config.macros !== undefined && config.macros.length > 0) {
+    config.macros = preprocessMacros(config.macros);
+    content = replaceMacros(content, config.macros);
+  }
   // Replace __FILE__ by the file name
-  content = content.replace(/__FILE__/g, this.resourcePath);
+  content = replaceFilename(content, this.resourcePath);
   // Replace __LINE__ by the line number
-  content = content
-              .split('\n')
-              .map(function (line, index) {
-                return line.replace(/__LINE__/g, index + 1);
-              })
-              .join('\n');
+  content = replaceLinenumber(content);
   // TODO(JD): __FUNCTION__
 
   if (query.emitFile === undefined || query.emitFile) {
     this.emitFile(this.resourcePath, content);
   }
   return content;
-}
+};
 module.exports.raw = true;
--- test/basic.spec.js
+++ test/basic.spec.js
@@ -1,46 +1,72 @@
-var should = require("should");
-var path = require("path");
-var preprocessorLoader = require("../");
+var should = require('should');
+var path = require('path');
+var preprocessorLoader = require('../');
 
 function run(resourcePath, query, content, expected) {
-  content = content || new Buffer("1234");
+  content = content || new Buffer('1234');
   var file = null;
   var context = {
     resourcePath: resourcePath,
-    query: "?" + query,
+    query: `?${query}`,
     options: {
-      context: "/this/is/the/context"
+      context: '/this/is/the/context',
     },
-    emitFile: function(url, content2) {
+    emitFile: function (url, content2) {
       content2.should.be.eql(expected);
       file = url;
-    }
+    },
   };
 
-  var result = preprocessorLoader.call(context, content)
+  var result = preprocessorLoader.call(context, content);
 
   return {
     file: file,
-    result: result
-  }
+    result: result,
+  };
 }
 
 function test(expected, resourcePath, query, content) {
   run(resourcePath, query, content, expected).file.should.be.eql(resourcePath);
 }
 
-describe("preprocessor-loader", function() {
-  it("should replace __FILE__ by the file name", function() {
-    let content = 'g__FILE_____ae643Y$%J&^55W#$^jh__FILE__s&^ERW%^U3__FILE__q4b';
-    let filename = 'test.js';
-    let result = 'g' + filename + '___ae643Y$%J&^55W#$^jh' +
-      filename + 's&^ERW%^U3' + filename + 'q4b';
-    test(result, filename, "", content);
+describe('preprocessor-loader', () => {
+  it('should replace __FILE__ by the file name', () => {
+    const content = 'g__FILE_____ae643Y$%J&^55W#$^jh__FILE__s&^ERW%^U3__FILE__q4b';
+    const filename = 'test.js';
+    const result = `g${filename}___ae643Y$%J&^55W#$^jh${filename}s&^ERW%^U3${filename}q4b`;
+    test(result, filename, '', content);
   });
-  it("should replace __LINE__ by the line number", function() {
-    let content = 'g__LINE_____ae\n3Y$%J&^55W#$^jh__LINE__s&^\n\nW%^\n3__LINE__q4b';
-    let filename = 'test.js';
-    let result = 'g1___ae\n3Y$%J&^55W#$^jh2s&^\n\nW%^\n35q4b';
-    test(result, filename, "", content);
+  it('should replace __LINE__ by the line number', () => {
+    const content = 'g__LINE_____ae\n3Y$%J&^55W#$^jh__LINE__s&^\n\nW%^\n3__LINE__q4b';
+    const filename = 'test.js';
+    const result = 'g1___ae\n3Y$%J&^55W#$^jh2s&^\n\nW%^\n35q4b';
+    test(result, filename, '', content);
+  });
+  it('should replace a user defined macros by their definitions', () => {
+    const content = 'This is a test\nMYMACRO\nEnd of file';
+    const filename = 'test.js';
+    const definition = 'Bla bla\n\n***';
+    const result = `This is a test\n${definition}\nEnd of file`;
+    test(result, filename, 'config=./test/basic.spec.json', content);
+  });
+  it('should replace a user defined macros without parameter by their definitions', () => {
+    const content = 'This is a test\nNO_PARAM_MACRO()\nEnd of file';
+    const filename = 'test.js';
+    const definition = 'noParamMacro()';
+    const result = `This is a test\n${definition}\nEnd of file`;
+    test(result, filename, 'config=./test/basic.spec.json', content);
+  });
+  it('should replace a user defined macros with parameters by their definitions', () => {
+    const content = 'This is a test\nqweqweMAX(5,9)\nEnd of file';
+    const filename = 'test.js';
+    const definition = '5 > 9 ? 5 : 9';
+    const result = `This is a test\nqweqwe${definition}\nEnd of file`;
+    test(result, filename, 'config=./test/basic.spec.json', content);
+  });
+  it('should be able to replace __FILE__ and __LINE__ within MACRO', () => {
+    const content = '\n\n\nLOG_INFO("this is a " + variable)';
+    const filename = 'test.js';
+    const result = '\n\n\nconsole.log("this is a " + variable + \' (test.js:4)\');';
+    test(result, filename, 'config=./test/basic.spec.json', content);
   });
 });
